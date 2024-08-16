b39f7b83c8a3f24c66017404fd398f3d7076eb47
prafullKrRj <prafullkumar384@gmail.com>
2024-08-17 00:06:53 +0530

    Update Edit

--- notes/NotesRoutes.js
+++ notes/NotesRoutes.js
@@ -8,7 +8,7 @@ const notesRouter = express.Router();
 notesRouter.get('/getAll', authMiddleware, getNotes);
 notesRouter.post('/create', authMiddleware, createNote);
 notesRouter.delete('/delete/:id', authMiddleware, deleteNote);
-notesRouter.put('/update/:id', authMiddleware, updateNote);
+notesRouter.put('/update', authMiddleware, updateNote);
 notesRouter.get('/get/:id', authMiddleware, getNote);
 notesRouter.post('/deleteMany', authMiddleware, deleteManyNotes);
 export default notesRouter;
